d4f53150d33e96b5edb2634be90d937e995fd53f
Samaya Gurung <samayagurung99@gmail.com>
2023-10-12 11:38:22 +0545

    feat: added yarn, thirdparty dependency and created navbar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { ToastContainer} from 'react-toastify';
+import { Route, Routes } from "react-router-dom";
+import NavBar from './components/Navbar';
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <NavBar/>
+    <Routes>
+      <Route/>
+    </Routes>
+   
+
+    <ToastContainer />
+    </>
+    
+  )
+}
+
+export default App
