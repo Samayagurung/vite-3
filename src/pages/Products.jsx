26c121e642d9322694fbf1f87a541a010ab8a7be
Samaya Gurung <samayagurung99@gmail.com>
2023-10-12 13:29:26 +0545

    feat: added addModal component to add products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import ProductList from "../components/ProductList";
 import axios from "axios";
+import AddModal from "../components/AddModal";
 
 const Products = () => {
   const URL = import.meta.env.VITE_BACKEND_URL;
 
   const [product, setProduct] = useState([]);
+  const [addProduct, setAddProduct] = useState([]);
+
+  const [addShow, setAddShow] = useState(false);
 
   useEffect(() => {
     const getProduct = async () => {
@@ -20,13 +24,48 @@ const Products = () => {
     getProduct();
   }, []);
 
+  const addHandle = (e) => {
+    e.preventDefault();
+    // console.log('clicked')
+    setAddShow(true);
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setAddProduct((prev) => {
+      // console.log({...prev,[e.target.name]:e.target.value})
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const addHandler = (e) => {
+    e.preventDefault();
+    // console.log("clicked")
+    setProduct([addProduct, ...product]);
+  };
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    setAddShow(false);
+  };
+
   return (
     <>
+      <button className="btn btn-outline-dark mt-3 ms-3" onClick={addHandle}>
+        {" "}
+        Add Product
+      </button>
       <div className="d-flex flex-wrap justify-content-center">
         {product.map((item) => {
           return <ProductList prodX={item} />;
         })}
       </div>
+      <AddModal
+        addShow={addShow}
+        handleClose={handleClose}
+        handleChange={handleChange}
+        addHandler={addHandler}
+      />
     </>
   );
 };
